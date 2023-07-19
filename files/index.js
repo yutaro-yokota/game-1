51b1600ce3583edd66e7728c83cfc65241bf4781
yokota-yutaro <yokoyuu530@gmail.com>
2023-07-19 23:12:05 +0900

    ファイルの追加

--- files/index.js
+++ files/index.js
@@ -1,17 +1,21 @@
 let canvas, g;
-
+let charaX, charaY, charaImage,charaImageSrc
 onload = function() {
   // 描画コンテキストの取得
   canvas = this.document.getElementById("gamecanvas");
   g = canvas.getContext("2d");
-  console.log("表示された");
   // 初期化
   init();
   //　ゲームループの設定: 60fps
   setInterval("gameloop()", 16);
 }
 
-function init() {}
+function init() {
+  charaX = 100;
+  charaY = 400;
+  charaImage = new Image();
+  charaImageSrc = "../images/test.png"
+}
 
 function keydown(e) {}
 
@@ -20,6 +24,21 @@ function gameloop() {
   draw();
 }
 
-function update() {}
+function update() {
+  charaX = charaX + 2;
+}
 
-function draw() {}
+function draw() {
+  // 背景
+  g.fillStyle = "rgb(0,0,0)";
+  g.fillRect(0,0,480,480);
+
+  // キャラ描画
+  g.drawImage (
+    charaImage,
+    //座標を画像の中心にする
+    charaX - charaImage.width /2,
+    charaY - charaImage.height /2,
+
+  )
+}
